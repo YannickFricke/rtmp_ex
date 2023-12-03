1ec407b89d40a5d5998a5e1ebf11b6b2179ef2f6
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-03 23:47:47 +0100

    refactor(rtmp): added the socket/0 alias
    The `RTMP.socket/0` type is a re-export of the gen_tcp socket type.

--- lib/r_t_m_p.ex
+++ lib/r_t_m_p.ex
@@ -2,4 +2,9 @@ defmodule RTMP do
   @moduledoc """
   Documentation for `RTMP`.
   """
+
+  @typedoc """
+  A re-export of the gen_tcp socket type.
+  """
+  @type socket() :: :gen_tcp.socket()
 end
