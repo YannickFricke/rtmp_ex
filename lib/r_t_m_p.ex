7a6583f061a9c589ee4a95e4497e592b255637dc
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-03 23:48:26 +0100

    refactor(rtmp): added the RTMP.default_port/0 function
    The function returns the default port (1935) of the RTMP protocol.

--- lib/r_t_m_p.ex
+++ lib/r_t_m_p.ex
@@ -7,4 +7,17 @@ defmodule RTMP do
   A re-export of the gen_tcp socket type.
   """
   @type socket() :: :gen_tcp.socket()
+
+  @doc """
+  Returns the default port (1935) for the RTMP protocol.
+
+  ## Examples
+
+  ```elixir
+  iex> RTMP.default_port()
+  1935
+  ```
+  """
+  @spec default_port() :: 1935
+  def default_port, do: 1935
 end
