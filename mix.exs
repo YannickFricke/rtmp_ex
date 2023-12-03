5265f5523866bb3209807c7d0580134f8dec5d64
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-03 23:27:26 +0100

    build(mix): set the version to 0.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RTMP.MixProject do
   def project do
     [
       app: :r_t_m_p,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
