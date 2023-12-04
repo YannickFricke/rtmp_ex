0e809379b3dc8c0c8bc388bf8be3fffe3e7db179
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-04 20:47:33 +0100

    feat: added the basic ClientConnection module

---
+++ lib/r_t_m_p/client_connection.ex
@@ -0,0 +1,27 @@
+defmodule RTMP.ClientConnection do
+  @moduledoc false
+
+  use GenServer, restart: :temporary
+
+  @name __MODULE__
+
+  @type state() :: %{
+          socket: RTMP.socket(),
+          ip: :inet.ip_address(),
+          port: :inet.port_number()
+        }
+
+  def start_link(options), do: GenServer.start_link(@name, options)
+
+  @spec init(options :: Keyword.t()) :: {:ok, state()}
+  def init(options) do
+    client_socket = Keyword.get(options, :socket)
+    client_ip = Keyword.get(options, :ip_address)
+    client_port = Keyword.get(options, :port)
+
+    {
+      :ok,
+      %{socket: client_socket, ip: client_ip, port: client_port}
+    }
+  end
+end
