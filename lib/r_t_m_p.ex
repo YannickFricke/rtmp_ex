aa7bbe464bb1041f0210e166b1a4bf95fff9e13e
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-04 14:22:52 +0100

    feat: added the RTMP.ip_to_string/1 utility function
    The function accepts an IPv4 or IPv6 address and returns the string
    representation of it.
    
    The IP address needs to be in the format of the :inet.ip_address() type.

--- lib/r_t_m_p.ex
+++ lib/r_t_m_p.ex
@@ -33,4 +33,28 @@ defmodule RTMP do
   """
   @spec default_timeout() :: non_neg_integer()
   def default_timeout, do: :timer.seconds(5)
+
+  @doc """
+  Stringifies the given IP adress
+
+  ## Examples
+
+  Stringifying an IPv4 address
+
+  ```elixir
+  iex> RTMP.ip_to_string({127, 0, 0, 1})
+  "127.0.0.1"
+  ```
+
+  Stringifying an IPv8 address
+
+  ```elixir
+  iex> RTMP.ip_to_string({0, 0, 0, 0, 0, 0, 0, 1})
+  "0:0:0:0:0:0:0:1"
+  ```
+  """
+  @spec ip_to_string(ip_address :: :inet.ip_address()) :: binary()
+  def ip_to_string({_, _, _, _} = ip_v4_address), do: ip_v4_address |> Tuple.to_list() |> Enum.join(".")
+
+  def ip_to_string({_, _, _, _, _, _, _, _} = ip_v6_address), do: ip_v6_address |> Tuple.to_list() |> Enum.join(":")
 end
