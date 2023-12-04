619f75912aa88fd4a8c3e64bd5c333bae292ffa4
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-04 21:23:15 +0100

    added the basic example

---
+++ example/lib/example/client_handler.ex
@@ -0,0 +1,17 @@
+defmodule Example.ClientHandler do
+  use RTMP.ClientHandler
+
+  require Logger
+
+  def on_tcp_connect(ip_address, port) do
+    ip_address_string = RTMP.ip_to_string(ip_address)
+
+    Logger.info("[RTMP] Got new connection from #{ip_address_string}:#{port}")
+  end
+
+  def on_client_disconnect(ip_address, port, _metadata) do
+    ip_address_string = RTMP.ip_to_string(ip_address)
+
+    Logger.info("[RTMP] Client #{ip_address_string}:#{port} disconnected")
+  end
+end
