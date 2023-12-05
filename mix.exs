db4626ce9ddc29ed4e2f714f43fa06967da11190
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-05 16:22:23 +0100

    build(mix): set the version 1.14 in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RTMP.MixProject do
     [
       app: :r_t_m_p,
       version: "0.0.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
