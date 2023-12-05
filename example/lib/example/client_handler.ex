e7dd0f49f8ca50ab86824090cfbcfb33f19e2ef9
Yannick Fricke <yannickfricke@googlemail.com>
2023-12-05 17:24:34 +0100

    chore(example): the ClientHandler should not use the RTMP prefix

--- example/lib/example/client_handler.ex
+++ example/lib/example/client_handler.ex
@@ -6,12 +6,12 @@ defmodule Example.ClientHandler do
   def on_tcp_connect(ip_address, port) do
     ip_address_string = RTMP.ip_to_string(ip_address)
 
-    Logger.info("[RTMP] Got new connection from #{ip_address_string}:#{port}")
+    Logger.info("Got new connection from #{ip_address_string}:#{port}")
   end
 
   def on_client_disconnect(ip_address, port, _metadata) do
     ip_address_string = RTMP.ip_to_string(ip_address)
 
-    Logger.info("[RTMP] Client #{ip_address_string}:#{port} disconnected")
+    Logger.info("Client #{ip_address_string}:#{port} disconnected")
   end
 end
